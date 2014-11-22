2146ab8e17bacd66a6773eb25d1f45b2087a4962
Andreas Gassmann <andreas@andreasgassmann.ch>
2014-11-22 16:20:44 +0100

    everything

--- todomvc/model.py
+++ todomvc/model.py
@@ -1,18 +1,57 @@
-
 from flask.ext.sqlalchemy import SQLAlchemy
 from application import app
 
 
 db = SQLAlchemy(app)
 
-class Item(db.Model):
+class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(80), unique=True)
-    description = db.Column(db.String(120), nullable=True, unique=True)
+    todos = db.relationship("Todo", uselist=False, backref="user")
 
-    def __init__(self, name, description=None):
+    def __init__(self, name):
         self.name = name
-        self.description = description
 
     def __repr__(self):
         return '<Item %r>' % self.name
+
+    def to_dict(self):
+        return {"id": self.id, "name": self.name}
+
+class Todo(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    subject = db.Column(db.String(80))
+    completed = db.Column(db.Boolean, nullable=True)
+    time_spans = db.relationship("TodoTime", uselist=True, backref="todo")
+
+
+    def __init__(self, user, subject, completed=False):
+        self.user = user
+        self.subject = subject
+        self.completed = completed
+
+    def __repr__(self):
+        return '<Item %r>' % self.name
+
+    def to_dict(self):
+        return {"id": self.id, "subject": self.subject, "completed": self.completed}
+        #"times": [time_span.to_dict() for time_span in self.time_spans]}
+
+class TodoTime(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    todo_id = db.Column(db.Integer, db.ForeignKey('todo.id'))
+    start = db.Column(db.TIMESTAMP)
+    end = db.Column(db.TIMESTAMP, nullable=True)
+
+    def __init__(self, todo, start, end):
+        self.todo = todo
+        self.start = start
+        self.end = end
+
+    def __repr__(self):
+        return '<Item %r>' % self.id
+
+    def to_dict(self):
+        return {"id": self.id, "start": self.start, "end": self.end}
+        return {"id": self.id, "start": self.start, "end": self.end}
--- todomvc/todomvc.py
+++ todomvc/todomvc.py
@@ -1,3 +1,10 @@
+from datetime import datetime, timedelta
+import logging
+from random import random
+
+logging.basicConfig()
+logging.getLogger('sqlalchemy.engine').setLevel(logging.INFO)
+
 from flask import request, jsonify, json
 
 from storages import *
@@ -12,46 +19,167 @@ store = ToDoStorage()
 def api():
     return 'This is the api for our little project'
 
-
 @app.route('/todos', methods=['GET', 'POST'])
 def todos():
-    if request.method == 'GET':
-        todo_list = store.list()
-        return json.dumps(todo_list)
+    # Get username from cookies
+    username = request.cookies.get('username')
+    # If no username cookie is set, send error
+    if not username:
+        return "Please log in first", 403
     else:
-        new_todo = request.get_json()
-        return jsonify(id=store.create(new_todo))
+        # Check if user set in cookies is valid
+        user = User.query.filter_by(name=username).first()
+        # If no user is found, send error
+        if not user:
+            return "Username not found", 404
+
+    # Handle GET requests
+    if request.method == 'GET':
+        # Get all ToDos of a user
+        todo_list = db.session.query(Todo).options(db.joinedload(Todo.time_spans)).filter(Todo.user_id == user.id)
+        # Return a list of all ToDos of a user
+        return json.dumps(list(todo.to_dict() for todo in todo_list))
+    else:
+        # Load POST json object into new_todo
+        new_todo = request.json
+        # Check if new_todo is set, if not, return error
+        if not new_todo:
+            return "first", 400
+        # Check if "subject" is set, if not, return error
+        if "subject" not in new_todo:
+            return "second", 400
+
+        # Create a new ToDo-Item
+        item = Todo(user, new_todo["subject"], new_todo.get("finished"))
+        # Add new Todo-Item to DB-Queue
+        db.session.add(item)
+        # Execute SQL queries
+        db.session.commit()
+        # Return new item in JSON format
+        return jsonify(item.to_dict())
 
 
 @app.route('/todos/<int:todo_id>', methods=['GET', 'PUT', 'DELETE'])
 def todo(todo_id):
-    if request.method == 'GET':
-        return jsonify(store.get(todo_id))
-    elif request.method == 'PUT':
-        new_todo = request.get_json()
-        if todo.id != todo_id:
-            return "INVALID ID"
-        else:
-            store.update(todo)
-            return "UPDATED"
+    # Get username from cookies
+    username = request.cookies.get('username')
+    # If no username cookie is set, send error
+    if not username:
+        return "Please log in first", 403
     else:
-        store.delete(todo_id)
-        return 'DELETED'
+        # Check if user set in cookies is valid
+        user = User.query.filter_by(name=username).first()
+        # If no user is found, send error
+        if not user:
+            return "Username not found", 404
 
+    # Get specific ToDo
+    item = Todo.query.options(db.joinedload(Todo.time_spans)).get(todo_id)
+    # Check if result is not empty and returned Item is linked to logged in user, if not, send error
+    if item is None or not (item.user_id == user.id):
+        return "Not Found", 404
 
-@app.route('/login', methods=['POST'])
-def login():
-    user = request.get_json()
-    # TODO: Set Cookie
+    # Handle GET requests
+    if request.method == 'GET':
+        # Send item as JSON
+        return jsonify(item.to_dict())
+    # Handle PUT request
+    elif request.method == 'PUT':
+        # Load POST json object into new_todo
+        new_todo = request.json
+        # Check if new_todo is set, if not, return error
+        if not new_todo:
+            return "first", 400
+        # Check if "subject" is set, if not, return error
+        if "subject" not in new_todo:
+            return "second", 400
+
+        item.name = new_todo["subject"]
+        item.description = new_todo.get("completed")
+
+        db.session.commit()
+        return jsonify(item.to_dict())
+    else:
+        db.session.query(Todo).filter(Todo.id == todo_id).delete()
+        db.session.commit()
+
+        return 'Item deleted'
+
+@app.route('/todos/<int:todo_id>/times', methods=['GET', 'POST'])
+def todoTimes(todo_id):
+
+    # Get username from cookies
+    username = request.cookies.get('username')
+    # If no username cookie is set, send error
+    if not username:
+        return "Please log in first", 403
+    else:
+        # Check if user set in cookies is valid
+        user = User.query.filter_by(name=username).first()
+        # If no user is found, send error
+        if not user:
+            return "Username not found", 404
+
+    # Get specific ToDo
+    item = Todo.query.options(db.joinedload(Todo.time_spans)).get(todo_id)
+    # Check if result is not empty and returned Item is linked to logged in user, if not, send error
+    if item is None or not (item.user_id == user.id):
+        return "Not Found", 404
+
+    # Handle GET requests
+    if request.method == 'GET':
+        # Send item as JSON
+        return json.dumps(item.to_dict().get("times"))
+    # Handle PUT request
+    elif request.method == 'POST':
+        # Load POST json object into new_todo
+        new_todo_time = request.json
+        # Check if new_todo_time is set, if not, return error
+        if not new_todo_time:
+            return "first", 400
+        # Check if "start" is set, if not, return error
+        if "start" not in new_todo_time:
+            return "No start time specified", 400
+        # Check if "end" is set, if not, return error
+        if "end" not in new_todo_time:
+            return "No end time specified", 400
+
+        todoTime = TodoTime(item, new_todo_time["start"], new_todo_time["end"])
+        db.session.add(todoTime)
+        db.session.commit()
+        return jsonify(todoTime.to_dict())
+
+@app.route('/login/<string:username>', methods=['GET', 'POST'])
+def login(username):
+
+    user = User.query.filter_by(name=username).first()
+    if not user:
+        user = User(username)
+        db.session.add(user)
+        db.session.commit()
+
+    response = app.make_response(json.dumps(user.to_dict()))
+    response.set_cookie('username', value=username)
+    return response
+
+@app.route('/logout', methods=['GET', 'POST'])
+def logout():
+    response = app.make_response("Logged out")
+    response.set_cookie('username', '', expires = 0)
+    return response
 
 
 #The following routes are to facilitate debugging
-@app.route('/debug/populate', methods=['POST'])
+@app.route('/debug/populate', methods=['GET', 'POST'])
 def populate():
     todo_list = ['Arrive at venue', 'Listen to tutor', 'Do the Tutorial', 'Eat Pizza', 'Work on the project', 'Win']
     db.create_all()
+    user = User("Superman")
     for todo in todo_list:
-        item = Item(todo)
+        item = Todo(user, todo)
+        for i in range(0, int(round(random()*10))):
+            todoTime = TodoTime(item, datetime.now() + timedelta(minutes=round(random()*100)), datetime.now() + timedelta(minutes=round(random()*100)))
+            db.session.add(todoTime)
         db.session.add(item)
     db.session.commit()
     return jsonify(length=len(todo_list))
